5755008a35dbff2404805c3859fe944ff56f8ea4
welli363 <toncosta08@gmail.com>
2026-04-09 11:19:23 -0300

    COMMIT

--- src/services/livro.service.js
+++ src/services/livro.service.js
@@ -1,54 +1,42 @@
 // SERVICE: Aqui mora a lógica de negócio da aplicação.
 // Esta camada não conhece Express, não conhece req, não conhece res.
 // Simulação do acervo — em breve será uma query no Postgres
-const acervo = [
-  {
-    id: 1,
-    titulo: 'O Senhor dos Anéis',
-    autor: 'J.R.R. Tolkien',
-    disponivel: true,
-  },
-  {
-    id: 2,
-    titulo: 'Altered Carbon',
-    autor: 'Richard K. Morgan',
-    disponivel: false,
-  },
-  {
-    id: 3,
-    titulo: "Assassin's Creed",
-    autor: 'Oliver Bowden',
-    disponivel: true,
-  },
-];
+const pool = require('../database/connection')
 
 // Lista todos os livros do acervo
 const listarTodosLivros = async () => {
-  return acervo;
+  try{
+    const resultado = await pool.query('SELECT * FROM livros ORDER BY id')
+    return resultado.rows
+  }catch (error) {
+    console.log('Erro ao digitar todos os livros', error.message)
+    throw error
+  }
 };
 
 // Busca um livro específico pelo ID
 const buscarLivroPorId = async (id) => {
-  const livro = acervo.find((item) => item.id === Number(id));
-  // Regra de negócio: se não existe, retorna null.
-  // O Controller decide o que fazer com o null.
-  return livro || null;
+  const resultado = await pool.query('SELECT * FROM livros WHERE id = $1', [
+    id,
+  ])
+   return resultado.rows[0]
 };
 
 // Criar um novo livro no acervo
-const criarLivro = async ({ titulo, autor }) => {
-  // Regra de negócio: título e autor são obrigatórios
-  if (!titulo || !autor) {
-    throw new Error('Título e autor são obrigatórios.');
+const criarLivro = async ({ titulo, autor, isbn, ano_publicacao, categoria_id }) => {
+  try{
+    const query = `INSERT INTO livros (titulo, autor, isbn, ano_publicacao, categoria_id ) VALUES ($1, $2, $3, $4, $5) RETURNING *`
+    const res = await pool.query([
+      titulo, 
+      autor, 
+      isbn,
+      ano_publicacao,
+      categoria_id,
+    ])
+  }catch (error){
+    
   }
-  const novoLivro = {
-    id: acervo.length + 1,
-    titulo,
-    autor,
-    disponivel: true,
-  };
-  acervo.push(novoLivro);
-  return novoLivro;
+  return res.rows[0]
 };
 
 module.exports = { listarTodosLivros, buscarLivroPorId, criarLivro };
